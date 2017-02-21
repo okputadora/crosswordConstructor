4822bec30def9ba637af1e1f264a14bf67381c55
okputadora <okputadora@gmail.com>
2017-02-21 14:45:18 -0500

    First

---
+++ crossword.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/xml; charset=utf-8" />
+  <!--ensures proper rendering and touch zooming on mobile devices-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Crossword</title>
+  <!--import google font-->
+  <link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
+  <!--import style sheet-->
+  <link rel="stylesheet" type = "text/css"
+  href="style.css">
+<head>
+  <script src="../jQuery.js"></script>
+  <script src="crossing.js"></script>
+<body id="main-content" class>
+  <div id="opt1"class="option">
+    <img class="optImg"/>
+    <h2 class="optTitle"> set grid</h2>
+    <p class="optDesc"> set your length and width first.</p>
+  </div>
+  <div id="opt2"class="option">
+    <img class="optImg"/>
+    <h2 class="optTitle"> build as you go</h2>
+    <p class="optDesc"> Just start connecting words, figure out the grid
+    size later.</p>
+  </div>
+</body>
