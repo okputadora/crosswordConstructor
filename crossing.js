531f4a1bbead08d3a18272b27935de99f4102f29
okputadora <okputadora@gmail.com>
2017-02-22 17:32:01 -0500

    Add event listener on "enter"

--- crossing.js
+++ crossing.js
@@ -1,24 +1,39 @@
+var enteringRow = true;
+var enteringCol = false;
+
+function displayGrid(){
+  var length = $("#len").val();
+  var width = $("#wid").val();
+  $("#gridsize").css("display", "none");
+  $("#crossword").css("display", "flex");
+  $("#tools").css("display", "flex");
+  for (i = 0; i < length; i++){
+    $("#grid").append("<div class='row' id='row" + i + "'></div>");
+    for (p = 0; p < width; p++){
+      $("#row" + i).append("<input class='box' id='box" + i + "" + p + "' maxlength='1'/>");
+    }
+  }
+  $("#row0").css("background-color", "blue !important");
+}
+
 $(document).ready(function(){
   $("#opt1").on("click", function(){
     $("#opt1").css("display", "none");
     $("#opt2").css("display", "none");
     $("#gridsize").css("display", "flex");
+    $("#len").focus();
   })
-
+  // allow enter or click to move to next screen\
+  $("#main-content").on("keyup", function(){
+    if (event.which === 13 && $("#gridsize").css("display") === "flex"){
+      displayGrid();
+    }
+  })
   $("#submit").on("click", function(){
-      var length = $("#len").val();
-      var width = $("#wid").val();
-      $("#gridsize").css("display", "none");
-      $("#crossword").css("display", "flex");
-      for (i = 0; i < length; i++){
-        $("#grid").append("<div class='row' id='row" + i + "'></div>");
-        for (p = 0; p < width; p++){
-          $("#row" + i).append("<input class='box' id='box" + i + "" + p + "' maxlength='1'/>");
-        }
-      }
+      displayGrid();
   })
 
-    $('#grid').on("keyup", ".box", function(e){
+    $('#grid').on("keyup", ".box", function(){
       if ($("#crossword").css("display") === "flex"){
         if (event.which === 32){
           $(this).css("background-color", "black");
--- toolbar.php
+++ toolbar.php
@@ -3,5 +3,18 @@
 </head>
 
 <div id="toolbar">
-  <h1 id="title">Crossword Constructor</h1>
+  <h1 id="title">Crossword Constructor |</h1>
+  <div id="tools">
+    <div class="tool"id="direction">
+      <div id="dir-row">row</div>
+      <div id="dir-col">column</div>
+    </div>
+    <div class="tool"id="autofill">
+      <div id="auto-word">auto-fill word</div>
+      <div id="auto-puzzle">auto-fill puzzle</div>
+    </div>
+    <div class="tool" id="clues">
+      <div id="auto-clue">auto-fill clues</div>
+    </div>
+  </div>
 </div>
