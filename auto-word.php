d3be5d2cff11cbd0b7e1cae578d33127ddde32ae
okputadora <okputadora@gmail.com>
2017-05-03 13:16:09 -0400

    php tells js when revision is needed

--- auto-word.php
+++ auto-word.php
@@ -1,14 +1,16 @@
 <?php include_once("connect.php");
+
 function calculate_median($arr) {
-$count = count($arr); //total numbers in array
-$middleval = floor(($count-1)/2); // find the middle value, or the lowest middle value
-if($count % 2) { // odd number, middle is the median
-    $median = $arr[$middleval];
-} else { // even number, calculate avg of 2 medians
-    $low = $arr[$middleval];
-    $high = $arr[$middleval+1];
-    $median = (($low+$high)/2);
-}
+  $count = count($arr); //total numbers in array
+  $middleval = floor(($count-1)/2); // find the middle value, or the lowest middle value
+  if ($count % 2) { // odd number, middle is the median
+      $median = $arr[$middleval];
+  }
+  else{ // even number, calculate avg of 2 medians
+      $low = $arr[$middleval];
+      $high = $arr[$middleval+1];
+      $median = (($low+$high)/2);
+  }
 return $median;
 }
   $partialWord = $_POST["word"];
@@ -22,17 +24,20 @@ return $median;
   $allFreqs = [];
   // get all of the possible words
   while ($row = mysqli_fetch_array($response)){
+    $wordGood = true;
     $freqs = [];
     $answer = $row['answer'];
     // for each crossing word
     $index = 0;
     foreach ($crosses as $value){
-      // if the word is complete skip
+      // if the word is already complete skip
       if ($value == "okputadora"){
-        $count = 1000;
+        // setting the count to 10000 so we don't accidentally navigate
+        // to this word next
+        $count = 10000;
       }
       else{
-        // insert the letter from $answer
+        // insert the letter from $answer into the corresponding crossword
         $length = strlen($value);
         for ($i = 0; $i < $length; $i++){
           // replace
@@ -43,26 +48,34 @@ return $median;
         }
         $query2 = "SELECT * FROM nytclues WHERE answer LIKE '" . $value . "'";
         $response2 = @mysqli_query($dbc, $query2);
+        // IF COUNT IS ZERO THIS $ANSWER WILL NOT DO
         $count = mysqli_num_rows($response2);
+        if ($count == 0){
+          $wordGood = false;
+          break;
+        }
       }
-      // IF COUNT IS ZERO THIS $ANSWER WILL NOT DO
       array_push($freqs, $count);
       $index += 1;
       // echo $answer;
     }
     // get min
-    $min = min($freqs);
-    array_push($allFreqs, $freqs);
-    array_push($mins, $min);
-    array_push($answers, $answer);
+    if ($wordGood){
+      $min = min($freqs);
+      array_push($allFreqs, $freqs);
+      array_push($mins, $min);
+      array_push($answers, $answer);
+    }
   }
-  // foreach($medians as $value){
-  //   echo $value . "-";
-  // }
-  // get the word with the best possible crosses
-  $maxIndex = array_search(max($mins),$mins);
-  $answer = $answers[$maxIndex];
-  $minMed = array_search(min($mins),$mins);
-  $minIndex = array_search(min($allFreqs[$minMed]), $allFreqs[$minMed]);
-  echo json_encode(array($answer, $minIndex));
+  // if we found a suitable word
+  if (sizeof($mins) > 1){
+    $maxIndex = array_search(max($mins),$mins);
+    $answer = $answers[$maxIndex];
+    $minIndex = array_search(min($allFreqs[$maxIndex]), $allFreqs[$maxIndex]);
+    echo json_encode(array($answer, $minIndex));
+  }
+  // we didn't find a suitable word
+  else{
+    echo json_encode(array("need to revise"));
+  }
 ?>
